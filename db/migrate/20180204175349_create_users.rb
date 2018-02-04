b495f1c2fd1bc77d5acf55f61dca185e038d1ac3
bamma_boy <vladimirsychkov@gmail.com>
2018-02-04 20:00:25 +0200

    User model

---
+++ db/migrate/20180204175349_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :surname
+      t.date :birthdate
+      t.string :address
+      t.integer :height
+      t.float :weight
+      t.float :goalweight
+
+      t.timestamps
+    end
+  end
+end
